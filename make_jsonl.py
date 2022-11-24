b310e8dd458c8d6f53b544d9bbf1beef40417e4a
Priyanshi Patel <priyanshi.patel@hotmail.com>
2022-11-23 21:26:16 -0500

    modified prompt

--- make_jsonl.py
+++ make_jsonl.py
@@ -3,9 +3,9 @@ import json
 
 if __name__=="__main__":
     csv_dataset = pd.read_csv('dataset.csv')
-    with open('dataset.jsonl', 'w') as out:
+    with open('new_dataset.jsonl', 'w') as out:
         for row in range(1,len(csv_dataset)):
-            prompt = "Question: " + csv_dataset["Question"][row] + "\nAnswer: " + csv_dataset["Answer"][row] + "\nQuestion: "
+            prompt = "Therapist: " + csv_dataset["Question"][row] + "\nPatient: " + csv_dataset["Answer"][row] + "\nTherapist: "
             completion = csv_dataset["Curious Question"][row]
             data = {"prompt":prompt,"completion":completion}
             json.dump(data, out)
