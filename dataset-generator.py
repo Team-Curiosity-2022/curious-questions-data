954bc0ef9a7416614e420f13e42ffe4ae0fecdc3
Valentina Manferrari <valentina.manferrari@gmail.com>
2023-03-18 12:44:03 -0400

    added comments to dataset generator pipeline to make it intuitive

--- dataset-generator.py
+++ dataset-generator.py
@@ -1,12 +1,25 @@
 import pandas as pd
 
+# Csv1 contains one empty col for Single Exchange storage 
+# and Curious Questions generated manually by the team for each single exchange
 csv1 = pd.read_csv("Curious Questions Prolific.csv")
+
+# Single Exchanges (Team Static Question + User response pairs) 
+# are collected through Prolific survey form and contained in csv2
 csv2 = pd.read_csv("Survey Responses Prolific.csv")
-print(csv2.columns[5:])
+# Skip first 5 columns as they contain reference data (eg. date, prolificID etc...)
+# Static questions are contained in the headers of columns 6 to 10
+static_questions = csv2.columns[5:] 
+
 i = 0
-for col in csv2.columns[5:]:
-    for response in csv2[col]:
-        csv1.loc[i,'Single Exchange'] = "Question: "+ col +" Answer: "+ response
+for question in static_questions:
+    for response in csv2[question]:
+        # merge Question-Answer pairs with Curious Questions in csv1 column1
+        csv1.loc[i,'Single Exchange'] = "Question: "+ question +" Answer: "+ response
         i+=1
+
+# Save to new file
 csv1.to_csv("dataset-prolific.csv",index=False)
+
+# For debugging purposes print the first 5 rows of new database
 print(csv1.head())
